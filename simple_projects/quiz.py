ae8faead974171a7a5d70cb4a697b2319d67a05e
Aarya Sarfare <aarya.work005@gmail.com>
2025-11-19 21:28:10 +0530

    adding a quiz to my mini projects, here I look through a nested dictionary of questions and make the user answer them, and provide points respectively

--- simple_projects/quiz.py
+++ simple_projects/quiz.py
@@ -1,10 +1,32 @@
-questions = {
+questions_dict = {
     'question1' : {
-        'question' : 'What is the first letter of word "Shirt"',
+        'Q' : 'What is the first letter of word "Shirt"',
         'answer' : 's'
     },
     'question2' : {
-        'question' : 'Where does a dog live?',
+        'Q' : 'Where does a dog live?',
         'answer' : 'kennel'
+    },
+    'question3' : {
+        'Q' : 'After how many years does a leap year come?',
+        'answer' : '4'
     }
 }
+
+points = 0
+
+print("\nWelcome To Quiz!\n")
+
+for key_of_question_dict, value_of_question_dict in questions_dict.items():
+
+    print(value_of_question_dict['Q'])
+    user_input = input("Answer: ").lower()
+    if user_input == value_of_question_dict['answer']:
+        print("Correct!")
+        points = points + 1
+    else:
+        print("Incorrect!")
+    print()
+    
+print(f"You've earned {points} points.")
+    
