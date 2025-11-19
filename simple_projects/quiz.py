1796972a3a9bd6a9ce8229e3619bd6a7ab275e3e
Aarya Sarfare <aarya.work005@gmail.com>
2025-11-19 19:19:56 +0530

    msg

--- simple_projects/quiz.py
+++ simple_projects/quiz.py
@@ -1,5 +1,10 @@
 questions = {
     'question1' : {
-        'question' : 'What is the first letter of word '
+        'question' : 'What is the first letter of word "Shirt"',
+        'answer' : 's'
+    },
+    'question2' : {
+        'question' : 'Where does a dog live?',
+        'answer' : 'kennel'
     }
 }
