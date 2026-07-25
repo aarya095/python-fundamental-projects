08532dca7847f0ed8066b4a6c4a809cb36f43204
Aarya Sarfare <aarya.work005@gmail.com>
2026-07-25 16:17:09 +0530

    Refactor input prompt and conditional statements for improved readability

--- rock_paper_scissor.py
+++ rock_paper_scissor.py
@@ -7,7 +7,9 @@ print("Enter 'e' to exit.\n")
 def is_win():
 
     while 1 == 1:
-        user_input = input("Select one, 'r' for rock, 'p' for paper, 's' for scissor: ").lower()
+        user_input = input(
+             "Select one, 'r' for rock, 'p' for paper, 's' for scissor: "
+             ).lower()
 
         if user_input == 'e':
              exit()
@@ -18,7 +20,8 @@ def is_win():
         if user_input == computer_choice:
             print("Its a tie! Play Again?\n")
 
-        elif (user_input == 'r' and computer_choice == 's') or (user_input == 'p' and computer_choice == 'r') \
+        elif (user_input == 'r' and computer_choice == 's') \
+              or (user_input == 'p' and computer_choice == 'r') \
             or (user_input == 's' and computer_choice == 'p'):
                 print("Congrats! You've won.\n")
 
